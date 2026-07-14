5d11f04fbb3fe9546b5c5f0b0c89e48f4ba536de
vanny <144558685+vanny-exe@users.noreply.github.com>
2026-07-14 14:02:16 -0400

    Teleport and Transition Script Added
    to do: figure out elevator script - when entering elevator from AHall, exit at Lobby. When entering Elevator from Lobby, exit at AHall. Include elevator animation (automate?)

---
+++ berrypickergame/Assets/MapTransition.cs
@@ -0,0 +1,56 @@
+using Unity.Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapTransition : MonoBehaviour
+{
+    [SerializeField] PolygonCollider2D mapBoundry; 
+    CinemachineConfiner2D confiner;
+    [SerializeField] Direction direction;
+    [SerializeField] Transform teleportTargetPosition;
+    [SerializeField] float additivePos = 2f;
+
+    enum Direction { Up, Down, Left, Right, Teleport }
+    
+
+    private void Awake()
+    {
+        confiner = FindObjectOfType<CinemachineConfiner2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            confiner.BoundingShape2D = mapBoundry;
+            UpdatePlayerPosition(collision.gameObject);
+        } 
+    }
+
+    private void UpdatePlayerPosition(GameObject player)
+    {
+        if(direction == Direction.Teleport){
+            player.transform.position = teleportTargetPosition.position;
+            return;
+        }
+
+        Vector3 newPos = player.transform.position;
+        switch (direction)
+        {
+            case Direction.Up:
+                newPos.y += additivePos; 
+                break;
+            case Direction.Down:
+                newPos.y -= additivePos; 
+                break;
+            case Direction.Left:
+                newPos.x -= additivePos; 
+                break;
+            case Direction.Right:
+                newPos.x += additivePos; 
+                break;
+        }
+        player.transform.position = newPos;
+    }
+}
